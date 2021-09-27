656e55859c92ebcbf4e83d50ee58e3c8e2947ae1
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2021-09-27 00:03:45 -0300

    css fix

--- src/components/Main/index.style.js
+++ src/components/Main/index.style.js
@@ -87,7 +87,7 @@ export const Thumbnail = styled.img`
     aspect-ratio: 1.7 /1;
 
     @media (max-width: 1024px) {
-        aspect-ratio: unset !important;
+        aspect-ratio: 1 / 1;
     }
 
     ${(props) =>
